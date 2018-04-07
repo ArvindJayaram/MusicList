c98c3642247aaff4f6b4183d1a6f1566a698b0e0
Christopher Buecheler <cwbuecheler@gmail.com>
2018-04-07 16:03:40 -0400

    Tutorial #13

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Musiclist' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+const testJSON = [
+  {
+    name: 'John Smith',
+    username: 'CaptainCode'
+  },
+  {
+    name: 'Jane Doe',
+    username: 'JaneyCakes'
+  }
+];
+
+router.get('/sendjson', (req, res, next) => {
+  res.json(testJSON)
 });
 
 module.exports = router;
